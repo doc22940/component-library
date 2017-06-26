91a3b13479874563efd3e4589f4fd77200ea5f40
Per Kristian Kummermo <per.kristian.kummermo@gmail.com>
2017-06-26 17:24:38 +0200

    Feature/nucleus (#1)
    * feat(nucleus): add nucleus config and build
    
    Also adds editorconfig and eslint for linting.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const connect = require('gulp-connect');
+
+const exec = require('child_process').exec;
+
+const buildFolder = 'dist';
+const componentsFolder = 'components';
+
+gulp.task('connect', function() {
+    connect.server({
+      root: buildFolder,
+      livereload: true
+    });
+});
+
+gulp.task('connect:reload', function () {
+    gulp.src([
+        `${ buildFolder }/**/*.html`,
+        `${ buildFolder }/**/*.css`
+    ]).pipe(connect.reload());
+});
+ 
+gulp.task('styleguide', (cb) => {
+    exec('nucleus', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.error(stderr);
+        cb(err);
+    });
+});
+ 
+gulp.task('sass', () => {
+    return gulp.src([`${ componentsFolder }/styleguide.scss`])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({force: true}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(`${ buildFolder }/styles`));
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch(
+        [`${ componentsFolder }/*.scss`, `${ componentsFolder }/**/*.scss`],
+        ['sass', 'styleguide', 'connect:reload']
+    );
+});
+
+gulp.task('styleguide:watch', ['styleguide', 'sass', 'connect', 'sass:watch'])
+gulp.task('default', ['styleguide', 'sass']);
